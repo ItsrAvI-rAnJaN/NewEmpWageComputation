e0719ae22ccae6afa153901bd4f25a37bc975fb3
ravi04ez <ezekie.em@gmail.com>
2022-07-06 23:38:55 +0530

    Welcome Messege to EmpwageComputation

---
+++ EmployeeWageComputations/EmpWageComputation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t############################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO EMPLOYEE WAGE COMPUTATIONS PROGRAM");
+            Console.WriteLine("\t\t\t\t#############################################################");
+            Console.ReadKey();
+        }
+    }
+}
